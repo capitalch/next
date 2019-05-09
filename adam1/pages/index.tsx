79fb9df697109401d5c42bc3f9688ed69d576171
capitalch <capitalch@gmail.com>
2019-05-09 18:02:52 +0530

    init

--- adam1/pages/index.tsx
+++ adam1/pages/index.tsx
@@ -2,24 +2,29 @@ import Link from 'next/link';
 import './index.scss';
 import axios from 'axios';
 
-function Index({ stars }) {
+function Index(props) {
+	// console.log(props);
+	console.log('Client:', props.stars);
 	return (
+		
 		<div>
 			<Link href="/about">
 				<a>About Page</a>
 			</Link>
 			<p>Hello Next.js</p>
-			<p>Next stars:{stars}</p>
+			<p>Next stars:{props.stars.name}</p>
 		</div>
 	);
 }
 Index.getInitialProps = async ({ req }) => {
+
 	const x = 0;
-	let data:any = {};
+	let d: any = {};
 	if (req) {
-		data = await axios.get('http://localhost:3002');
+		d = await axios.get('http://localhost:3002');
+		console.log(d.data);
 	}
-	return { stars: JSON.stringify(data.data) };
+	return { stars: d.data };
 };
 
 export default Index;
--- adam3/pages/index.js
+++ adam3/pages/index.js
@@ -1,22 +1,87 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
 
-export default () => (
-  <ul>
-    <li>
-      <Link href='/b' as='/a'>
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/a' as='/b'>
-        <a>b</a>
-      </Link>
-    </li>
-    <li>
-      <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
-        <a>post #2</a>
-      </Link>
-    </li>
-  </ul>
-)
+function Index(props) {
+	const [ dt, setDt ] = useState({});
+	// setDt(props.stars);
+	// console.log('Client:', props.stars);
+	console.log('Client:', dt);
+	return (
+		<ul>
+			<li>
+				<Link href="/b" as="/b">
+					<a>b</a>
+				</Link>
+			</li>
+			<li>
+				<Link href="/a" as="/a">
+					<a>a</a>
+				</Link>
+			</li>
+			<li>
+				<Link href={{ pathname: '/posts', query: { id: '2' } }} as="/posts/2">
+					<a>post #2</a>
+				</Link>
+			</li>
+		</ul>
+	);
+}
+Index.getInitialProps = async ({ req }) => {
+	const x = 0;
+	let d = {};
+	if (req) {
+		d = await axios.get('http://localhost:3002');
+		console.log(d.data);
+	}
+	return { stars: d.data };
+};
+
+export default Index;
+
+// export default () => (
+//   <ul>
+//     <li>
+//       <Link href='/b' as='/b'>
+//         <a>b</a>
+//       </Link>
+//     </li>
+//     <li>
+//       <Link href='/a' as='/a'>
+//         <a>a</a>
+//       </Link>
+//     </li>
+//     <li>
+//       <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
+//         <a>post #2</a>
+//       </Link>
+//     </li>
+//   </ul>
+// )
+/*
+function Index(props) {
+	console.log('Client:', props.stars);
+	return (
+		
+		<div>
+			<Link href="/about">
+				<a>About Page</a>
+			</Link>
+			<p>Hello Next.js</p>
+			<p>Next stars:{props.stars.name}</p>
+		</div>
+	);
+}
+Index.getInitialProps = async ({ req }) => {
+
+	const x = 0;
+	let d: any = {};
+	if (req) {
+		d = await axios.get('http://localhost:3002');
+		console.log(d.data);
+	}
+	return { stars: d.data };
+};
+
+export default Index;
+*/
