2c316427017fa5281e2454a1636652589dee40b6
SushantAgrawal <sagarwal@netwoven.com>
2019-05-20 09:03:49 +0530

    init

---
+++ xprofile/components/nav.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './nav.module.scss';
+// import './nav.css';
+
+const Nav = () => (
+    <div>
+        test
+        <style>
+            jsx {
+                `color: red;
+                margin: 10px;
+                `
+            }
+        </style>
+    </div>
+);
+
+export default Nav;
+/*
+
+*/
